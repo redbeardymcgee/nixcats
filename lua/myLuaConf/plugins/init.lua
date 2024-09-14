2ea214ee37ab7076e08a7bddd8641577ce1becb0
redbeardymcgee <redbeardymcgee@gmail.com>
2024-09-14 13:39:30 -0500

    remove basic defaults clutter

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -315,9 +315,6 @@ require("mini.surround").setup({
     highlight = 'gsh',      -- Highlight surrounding
     replace = 'gsr',        -- Replace surrounding
     update_n_lines = 'gsn', -- Update `n_lines`
-
-    suffix_last = 'l',      -- Suffix to search with "prev" method
-    suffix_next = 'n',      -- Suffix to search with "next" method
   },
 })
 require("mini.pairs").setup({
