547212ba65ad02ed10b5e84e22efef3e2a20b5c2
redbeardymcgee <redbeardymcgee@gmail.com>
2024-09-17 13:39:16 -0500

    fix markdown keybinds

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,71 +1,72 @@
-  require('glow').setup()
-  require('render-markdown').setup()
+require('glow').setup()
+require('render-markdown').setup()
 
-  vim.g.mkdp_auto_close = 0
-  vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview <CR>',
-    { noremap = true, desc = 'markdown preview' })
-  vim.keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop <CR>',
-    { noremap = true, desc = 'markdown preview stop' })
-  vim.keymap.set('n', '<leader>mt', '<cmd>MarkdownPreviewToggle <CR>',
-    { noremap = true, desc = 'markdown preview toggle' })
+vim.g.mkdp_auto_close = 0
+vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview <CR>',
+  { noremap = true, desc = 'markdown preview' })
+vim.keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop <CR>',
+  { noremap = true, desc = 'markdown preview stop' })
+vim.keymap.set('n', '<leader>mt', '<cmd>MarkdownPreviewToggle <CR>',
+  { noremap = true, desc = 'markdown preview toggle' })
 
-  require('markdown').setup({
-    on_attach = function(bufnr)
-      local map = vim.keymap.set
-      local opts = { buffer = bufnr }
-      map({ 'n', 'i' }, '<leader>ml', '<Cmd>MDListItemBelow<CR>', opts)
-      map({ 'n', 'i' }, '<leader>mL', '<Cmd>MDListItemAbove<CR>', opts)
-      map('n', '<leader>mc', '<Cmd>MDTaskToggle<CR>', opts)
-      map('x', '<leader>mc', ':MDTaskToggle<CR>', opts)
-    end,
+require('markdown').setup({
+  on_attach = function(bufnr)
+    local map = vim.keymap.set
+    local opts = { buffer = bufnr }
+    map('n', '<leader>ml', '<Cmd>MDListItemBelow<CR>', opts)
+    map('n', '<leader>mL', '<Cmd>MDListItemAbove<CR>', opts)
+    map('n', '<leader>mc', '<Cmd>MDTaskToggle<CR>', opts)
+    map('x', '<leader>mc', ':MDTaskToggle<CR>', opts)
+    map('n', '<leader>mr', '<Cmd>MDResetListNumbering<CR>', opts)
+  end,
+})
+
+if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
+  local map = vim.keymap.set
+
+  map("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", {
+    noremap = true,
+    silent = false,
   })
 
-  if require("zk.util").notebook_root(vim.fn.expand('%:p')) ~= nil then
-    local map = vim.keymap.set
+  map("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", {
+    noremap = true,
+    silent = false,
+    desc = "New note in buffer's parent dir with title from input"
+  })
 
-    map("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", {
+  map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", {
+    noremap = true,
+    silent = false,
+    desc = "New note in buffer's parent dir with title from selection"
+  })
+
+  map("v", "<leader>znc",
+    ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", {
       noremap = true,
       silent = false,
+      desc = "New note in buffer's parent dir with content from selection"
     })
 
-    map("n", "<leader>zn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", {
-      noremap = true,
-      silent = false,
-      desc = "New note in buffer's parent dir with title from input"
-    })
+  map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", {
+    noremap = true,
+    silent = false,
+    desc = "Open notes linking to current note"
+  })
+  -- Alternative for backlinks using pure LSP and showing the source context.
+  --map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
 
-    map("v", "<leader>znt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", {
-      noremap = true,
-      silent = false,
-      desc = "New note in buffer's parent dir with title from selection"
-    })
+  map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", {
+    noremap = true,
+    silent = false,
+    desc = "Open notes linked by the current note"
+  })
 
-    map("v", "<leader>znc",
-      ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", {
-        noremap = true,
-        silent = false,
-        desc = "New note in buffer's parent dir with content from selection"
-      })
+  map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = false })
 
-    map("n", "<leader>zb", "<Cmd>ZkBacklinks<CR>", {
-      noremap = true,
-      silent = false,
-      desc = "Open notes linking to current note"
-    })
-    -- Alternative for backlinks using pure LSP and showing the source context.
-    --map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
-
-    map("n", "<leader>zl", "<Cmd>ZkLinks<CR>", {
-      noremap = true,
-      silent = false,
-      desc = "Open notes linked by the current note"
-    })
-
-    map("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = false })
-
-    map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", {
-      noremap = true,
-      silent = false,
-      desc = "Code action for selection"
-    })
-  end
+  map("v", "<leader>za", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", {
+    noremap = true,
+    silent = false,
+    desc = "Code action for selection"
+  })
+end
