24c6f25cd414579c9d13671d7687c1cb4f2af681
redbeardymcgee <redbeardymcgee@gmail.com>
2024-12-14 10:17:27 -0600

    fix some plugin bindings

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -185,28 +185,17 @@ require('which-key').add {
 }
 
 require('yazi').setup({
+  open_multiple_tabs = true,
   open_for_directories = true,
-  -- open_multiple_tabs = true,
-
-  -- use_ya_for_events_reading = true,
-  -- highlight_groups = {
-  --   hovered_buffer_background = { bg = "#363a4f" },
-  -- },
-  -- hovered_buffer_in_same_directory = nil,
+  log_level = vim.log.levels.OFF,
 })
-km.set("n", "-",
-  function()
-    require("yazi").yazi()
-  end,
+km.set("n", "-", "<cmd>Yazi<cr>",
   {
     noremap = true,
     desc = 'Browse parent directory'
   }
 )
-km.set("n", "<leader>-",
-  function()
-    require("yazi").yazi(nil, vim.fn.getcwd())
-  end,
+km.set("n", "<leader>-", "<cmd>Yazi cwd<cr>",
   {
     noremap = true,
     desc = 'Browse current working directory'
@@ -345,9 +334,97 @@ require("flash").setup({
 })
 km.set({ "n", "x", "o" }, "<C-f>", function() require("flash").treesitter() end, { desc = "Flash Treesitter", })
 km.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
-km.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { noremap = true, silent = false, desc = "Lazygit" })
-km.set("n", "<leader>gf", "<cmd>LazyGitFilterCurrentFile<cr>",
-  { noremap = true, silent = false, desc = "Lazygit filter current file" })
+
+require("snacks").setup({
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    quickfile = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        -- wo = { wrap = true } -- Wrap notifications
+      }
+    }
+})
+--   { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+--   { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+--   { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+--   { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+--   { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+--   { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+--   { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+--   { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+--   { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+--   { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+--   { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+--   { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+--   { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+--   { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+--   { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+--   { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+--   { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+--   {
+--     "<leader>N",
+--     desc = "Neovim News",
+--     function()
+--       Snacks.win({
+--         file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+--         width = 0.6,
+--         height = 0.6,
+--         wo = {
+--           spell = false,
+--           wrap = false,
+--           signcolumn = "yes",
+--           statuscolumn = " ",
+--           conceallevel = 3,
+--         },
+--       })
+--     end,
+--   }
+-- },
+
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "VeryLazy",
+--   callback = function()
+--     -- Setup some globals for debugging (lazy-loaded)
+--     _G.dd = function(...)
+--       Snacks.debug.inspect(...)
+--     end
+--     _G.bt = function()
+--       Snacks.debug.backtrace()
+--     end
+--     vim.print = _G.dd -- Override print to use snacks for `:=` command
+--
+--     -- Create some toggle mappings
+--     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+--     Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+--     Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+--     Snacks.toggle.diagnostics():map("<leader>ud")
+--     Snacks.toggle.line_number():map("<leader>ul")
+--     Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+--     Snacks.toggle.treesitter():map("<leader>uT")
+--     Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+--     Snacks.toggle.inlay_hints():map("<leader>uh")
+--     Snacks.toggle.indent():map("<leader>ug")
+--     Snacks.toggle.dim():map("<leader>uD")
+--   end,
+-- })
+
+-- km.set("n", "<leader>gg", require("tsugit").toggle(),
+--   { noremap = true, silent = false, desc = "Lazygit" })
+-- km.set("n", "<leader>gf",
+--   function()
+--     require("tsugit").toggle_for_file(vim.api.nvim_buf_get_name(0))
+--   end,
+--   { noremap = true, silent = false, desc = "Lazygit file commits" })
 
 require('yanky').setup()
 km.set({ "n", "x" }, "<leader>p",
@@ -440,3 +517,5 @@ require('Comment').setup {
 -- require('mini-align').setup()
 
 -- require('image').setup()
+
+
