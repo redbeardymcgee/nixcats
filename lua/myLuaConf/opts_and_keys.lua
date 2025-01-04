d873d8e041ab16931d990fae2f702d76afd61df1
redbeardymcgee <redbeardymcgee@gmail.com>
2025-01-04 09:42:31 -0600

    move keybinds for snacks.nvim

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -132,3 +132,39 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+vim.keymap.set("n", "<leader>Z",  function() Snacks.zen() end, { desc = "Toggle Zen Mode" })
+vim.keymap.set("n", "<leader>ZZ",  function() Snacks.zen.zoom() end, { desc = "Toggle Zoom" })
+
+vim.keymap.set("n", "<leader>.",  function() Snacks.scratch() end, { desc = "Toggle Scratch Buffer" })
+vim.keymap.set("n", "<leader>S",  function() Snacks.scratch.select() end, { desc = "Select Scratch Buffer" })
+vim.keymap.set("n", "<leader>n",  function() Snacks.notifier.show_history() end, { desc = "Notification History" })
+vim.keymap.set("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>cR", function() Snacks.rename.rename_file() end, { desc = "Rename File" })
+vim.keymap.set("n", "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse" })
+vim.keymap.set("n", "<leader>gb", function() Snacks.git.blame_line() end, { desc = "Git Blame Line" })
+vim.keymap.set("n", "<leader>gf", function() Snacks.lazygit.log_file() end, { desc = "Lazygit Current File History" })
+vim.keymap.set("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit" })
+vim.keymap.set("n", "<leader>gl", function() Snacks.lazygit.log() end, { desc = "Lazygit Log (cwd )" })
+vim.keymap.set("n", "<leader>un", function() Snacks.notifier.hide() end, { desc = "Dismiss All Notifications" })
+vim.keymap.set("n", "<c-/>",      function() Snacks.terminal() end, { desc = "Toggle Terminal" })
+vim.keymap.set("n", "]]",         function() Snacks.words.jump(vim.v.count1) end, { desc = "Next Reference" })
+vim.keymap.set("n", "[[",         function() Snacks.words.jump(-vim.v.count1) end, { desc = "Prev Reference" })
+
+vim.keymap.set(
+  "n",
+  "<leader>N",
+    function()
+      Snacks.win({
+        file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+        width = 0.6,
+        height = 0.6,
+        wo = {
+          spell = false,
+          wrap = false,
+          signcolumn = "yes",
+          statuscolumn = " ",
+          conceallevel = 3,
+        },
+      })
+    end,
+    { desc = "Neovim News" })
