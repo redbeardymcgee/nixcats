c833b02e2117d47b93bd00ddddd470570fe882d8
redbeardymcgee <redbeardymcgee@gmail.com>
2025-10-31 21:22:02 -0500

    discard useless keymaps

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -45,14 +45,6 @@ vim.o.smarttab = true
 -- NOTE: if you dont append to the previous cpoptions, which-key will throw many errors
 vim.o.cpoptions = (vim.o.cpoptions or '') .. 'I'
 vim.o.expandtab = true
--- this part of indent settings is currently
--- being handled by treesitter
--- vim.o.smartindent = true
--- vim.o.indentexpr = true
--- vim.o.autoindent = true
--- vim.o.tabstop = 4
--- vim.o.softtabstop = 4
--- vim.o.shiftwidth = 4
 
 -- stops line wrapping from being confusing
 vim.o.breakindent = true
@@ -87,31 +79,18 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
--- local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
--- vim.api.nvim_create_autocmd('TextYankPost', {
---   callback = function()
---     vim.highlight.on_yank()
---   end,
---   group = highlight_group,
---   pattern = '*',
--- })
-
 vim.g.netrw_liststyle = 0
 vim.g.netrw_banner = 0
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-vim.keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Moves Line Down' })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Moves Line Up' })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll Down' })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll Up' })
-vim.keymap.set("n", "n", "nzzzv", { desc = 'Next Search Result' })
-vim.keymap.set("n", "N", "Nzzzv", { desc = 'Previous Search Result' })
+-- See `:help km.set()`
+km.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Moves Line Down' })
+km.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Moves Line Up' })
+km.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll Down' })
+km.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll Up' })
+km.set("n", "n", "nzzzv", { desc = 'Next Search Result' })
+km.set("n", "N", "Nzzzv", { desc = 'Previous Search Result' })
 
 km.set("n", "<leader><leader>[", "<cmd>bprev<CR>", { desc = 'Previous buffer' })
 km.set("n", "<leader><leader>]", "<cmd>bnext<CR>", { desc = 'Next buffer' })
@@ -134,48 +113,13 @@ km.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 -- km.set("n", "<leader>gf", function() Snacks.lazygit.log_file() end, { desc = "Lazygit Current File History" })
 
 
--- vim.keymap.set(
---   "n",
---   "<leader>N",
---   function()
---     Snacks.win({
---       file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
---       width = 0.6,
---       height = 0.6,
---       wo = {
---         spell = false,
---         wrap = false,
---         signcolumn = "yes",
---         statuscolumn = " ",
---         conceallevel = 3,
---       },
---     })
---   end,
---   { desc = "Neovim News" })
+km.set("n", "<leader>gg", "<cmd>FloatermNew lazygit<cr>", { desc = "Lazygit" })
+km.set("n", "<leader>gl", "<cmd>FloatermNew lazygit log<cr>", { desc = "Lazygit Log (cwd )" })
+km.set("n", "<c-/>", "<cmd>FloatermToggle<cr>", { desc = "Toggle Terminal" })
 
--- vim.api.nvim_create_autocmd("User", {
---   pattern = "VeryLazy",
---   callback = function()
---     -- Setup some globals for debugging (lazy-loaded)
---     _G.dd = function(...)
---       Snacks.debug.inspect(...)
---     end
---     _G.bt = function()
---       Snacks.debug.backtrace()
---     end
---     vim.print = _G.dd -- Override print to use snacks for `:=` command
---
---     -- Create some toggle mappings
---     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
---     Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
---     Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
---     Snacks.toggle.diagnostics():map("<leader>ud")
---     Snacks.toggle.line_number():map("<leader>ul")
---     Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
---     Snacks.toggle.treesitter():map("<leader>uT")
---     Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
---     Snacks.toggle.inlay_hints():map("<leader>uh")
---     Snacks.toggle.indent():map("<leader>ug")
---     Snacks.toggle.dim():map("<leader>uD")
---   end,
--- })
+km.set("n", "-", "<cmd>FloatermNew yazi<cr>",
+  {
+    noremap = true,
+    desc = 'Browse parent directory'
+  }
+)
