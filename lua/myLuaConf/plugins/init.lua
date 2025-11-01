2812252773a3d60656c008dc616e279b1f865d06
redbeardymcgee <redbeardymcgee@gmail.com>
2025-11-01 09:36:38 -0500

    move lualine to own file

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -8,6 +8,7 @@ vim.cmd.colorscheme(colorschemeName)
 
 require('myLuaConf.plugins.completion')
 require('myLuaConf.plugins.dashboard')
+require('myLuaConf.plugins.lualine')
 require('myLuaConf.plugins.telescope')
 require('myLuaConf.plugins.toggleterm')
 require('myLuaConf.plugins.treesitter')
@@ -27,35 +28,6 @@ require('hlargs').setup {
 vim.cmd([[hi clear @lsp.type.parameter]])
 vim.cmd([[hi link @lsp.type.parameter Hlargs]])
 
-require('lualine').setup({
-  options = {
-    icons_enabled = false,
-    theme = colorschemeName,
-    component_separators = '|',
-    section_separators = '',
-  },
-  sections = {
-    lualine_c = {
-      {
-        'filename', path = 1, status = true,
-      },
-    },
-  },
-  inactive_sections = {
-    lualine_b = {
-      {
-        'filename', path = 3, status = true,
-      },
-    },
-    lualine_x = { 'filetype' },
-  },
-  tabline = {
-    lualine_a = { 'buffers' },
-    -- if you use lualine-lsp-progress, I have mine here instead of fidget
-    -- lualine_b = { 'lsp_progress', },
-    lualine_z = { 'tabs' }
-  },
-})
 require('fidget').setup({})
 
 -- indent-blank-line
