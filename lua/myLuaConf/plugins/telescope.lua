9ac58fb67d798e2b3619c26861b61f532f6eeff8
redbeardymcgee <redbeardymcgee@gmail.com>
2025-11-01 23:17:37 -0500

    fix some keybinding

--- lua/myLuaConf/plugins/telescope.lua
+++ lua/myLuaConf/plugins/telescope.lua
@@ -40,8 +40,8 @@ require('telescope').setup {
 }
 
 -- Enable telescope extensions, if they are installed
-pcall(require('telescope').load_extension, 'fzf')
-pcall(require('telescope').load_extension, 'ui-select')
+require('telescope').load_extension('fzf')
+require('telescope').load_extension('ui-select')
 require('telescope').load_extension('dap')
 require('telescope').load_extension('heading')
 require("telescope").load_extension("yank_history")
@@ -57,8 +57,8 @@ vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch curren
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>s', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
+vim.keymap.set('n', '<leader><leader>s', builtin.buffers, { desc = '[ ] [S]earch buffers' })
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
@@ -107,7 +107,7 @@ local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
     require('telescope.builtin').live_grep({
-      search_dirs = {git_root},
+      search_dirs = { git_root },
     })
   end
 end
