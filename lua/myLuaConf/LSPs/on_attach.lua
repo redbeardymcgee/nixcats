8004a429a643067a76a38ef54b3981d454668c1a
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-15 15:23:42 -0600

    remove duplicates

--- lua/myLuaConf/LSPs/on_attach.lua
+++ lua/myLuaConf/LSPs/on_attach.lua
@@ -39,10 +39,6 @@ return function(_, bufnr)
   nmap("K", vim.lsp.buf.hover, "Hover Documentation")
   nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-  -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
   -- Lesser used LSP functionality
   nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
   nmap(
@@ -58,11 +54,6 @@ return function(_, bufnr)
   nmap("<leader>Wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, "[W]orkspace [L]ist Folders")
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    vim.lsp.buf.format()
-  end, { desc = "Format current buffer with LSP" })
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
