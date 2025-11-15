ae22671398e55fd170d85d11a06e58bac0825b6d
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-14 18:07:40 -0600

    make lazygit terminals properly sized
    Terminal sizes are determined during toggleterm setup, which may occur
    when windows are very small if tmux splits are in use. I don't know if
    the same applies to nvim splits, but it isn't important.

--- lua/myLuaConf/plugins/tasks.lua
+++ lua/myLuaConf/plugins/tasks.lua
@@ -79,6 +79,12 @@ return {
         cmd = "lazygit",
         hidden = true,
         direction = "float",
+        float_opts = {
+          border = "curved",
+          width = math.floor(vim.o.columns * 0.8),
+          height = math.floor(vim.o.lines * 0.8),
+          title_pos = "center",
+        },
       })
 
       function _lazygit_toggle()
