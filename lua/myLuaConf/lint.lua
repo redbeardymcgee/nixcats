0760f6d4b406376d20cacb640c3fecc5b70e9bdc
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-15 23:17:23 -0600

    enable linters

--- lua/myLuaConf/lint.lua
+++ lua/myLuaConf/lint.lua
@@ -1,20 +1,23 @@
 require("lze").load({
   {
     "nvim-lint",
-    for_cat = "lint",
-    -- cmd = { "" },
-    event = "FileType",
-    -- ft = "",
-    -- keys = "",
-    -- colorscheme = "",
+    for_cat = "general.extra",
+    event = "BufEnter",
     after = function(plugin)
       local lint = require("lint")
       lint.linters_by_ft = {
-        -- NOTE: download some linters in lspsAndRuntimeDeps
-        -- and configure them here
-        -- markdown = {'vale',},
-        -- javascript = { 'eslint' },
-        -- typescript = { 'eslint' },
+        markdown = { "eslint" },
+        javascript = { "eslint" },
+        typescript = { "eslint" },
+        json = { "eslint" },
+        jsonc = { "eslint" },
+        json5 = { "eslint" },
+        astro = { "eslint" },
+        fish = { "fish" },
+        nix = { "nix" },
+        yaml = { "yq" },
+        bash = { "shellcheck" },
+        sh = { "shellcheck" },
       }
 
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
