633300b9d0277e76b3ef3b372af9dca909cfe86f
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-15 21:53:36 -0600

    enable better nixd lsp integration with my flake

--- flake.nix
+++ flake.nix
@@ -503,6 +503,12 @@
               # nix
               ''import ${pkgs.path} {}'';
             # or inherit nixpkgs;
+            nixos_options = ''
+              (builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options
+            '';
+            home_manager_options = ''
+              (builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options
+            '';
           };
         };
       };
