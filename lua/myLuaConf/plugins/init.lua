9b435702601e5b5c35f33666b5e3bde095ef32fb
redbeardymcgee <redbeardymcgee@proton.me>
2025-12-03 15:59:13 -0600

    alex lint

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -4,7 +4,7 @@ if not require("nixCatsUtils").isNixCats then
 end
 -- Could I lazy load on colorscheme with lze?
 -- sure. But I was going to call vim.cmd.colorscheme() during startup anyway
--- this is just an example, feel free to do a better job!
+-- this is only an example, feel free to do a better job!
 vim.cmd.colorscheme(colorschemeName)
 
 local ok, notify = pcall(require, "notify")
