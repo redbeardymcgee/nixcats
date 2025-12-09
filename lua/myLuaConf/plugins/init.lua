7349adff0d1c265cef6a43b679eedabe6968d75b
redbeardymcgee <redbeardymcgee@proton.me>
2025-12-08 18:50:42 -0600

    add insert mode bindings for splits

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -40,19 +40,23 @@ require("smart-splits").setup({
   },
   log_level = "trace",
 })
-vim.keymap.set("n", "<C-A-h>", require("smart-splits").resize_left)
-vim.keymap.set("n", "<C-A-j>", require("smart-splits").resize_down)
-vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
-vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
-vim.keymap.set("n", "<A-h>", require("smart-splits").move_cursor_left)
-vim.keymap.set("n", "<A-j>", require("smart-splits").move_cursor_down)
-vim.keymap.set("n", "<A-k>", require("smart-splits").move_cursor_up)
-vim.keymap.set("n", "<A-l>", require("smart-splits").move_cursor_right)
-vim.keymap.set("n", "<A-\\>", require("smart-splits").move_cursor_previous)
-vim.keymap.set("n", "<A-H>", require("smart-splits").swap_buf_left)
-vim.keymap.set("n", "<A-J>", require("smart-splits").swap_buf_down)
-vim.keymap.set("n", "<A-K>", require("smart-splits").swap_buf_up)
-vim.keymap.set("n", "<A-L>", require("smart-splits").swap_buf_right)
+vim.keymap.set({ "n", "i" }, "<C-A-h>", require("smart-splits").resize_left)
+vim.keymap.set({ "n", "i" }, "<C-A-j>", require("smart-splits").resize_down)
+vim.keymap.set({ "n", "i" }, "<C-A-k>", require("smart-splits").resize_up)
+vim.keymap.set({ "n", "i" }, "<C-A-l>", require("smart-splits").resize_right)
+vim.keymap.set({ "n", "i" }, "<A-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set({ "n", "i" }, "<A-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set({ "n", "i" }, "<A-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set({ "n", "i" }, "<A-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set(
+  { "n", "i" },
+  "<A-\\>",
+  require("smart-splits").move_cursor_previous
+)
+vim.keymap.set({ "n", "i" }, "<A-H>", require("smart-splits").swap_buf_left)
+vim.keymap.set({ "n", "i" }, "<A-J>", require("smart-splits").swap_buf_down)
+vim.keymap.set({ "n", "i" }, "<A-K>", require("smart-splits").swap_buf_up)
+vim.keymap.set({ "n", "i" }, "<A-L>", require("smart-splits").swap_buf_right)
 
 if nixCats("general.extra") then
   -- Dashboard
